e6af8219df396c64e33d630162e388d965ed85f0
githyuniiee <109260733+githyuniiee@users.noreply.github.com>
2022-10-28 15:42:56 +0900

    Add files via upload

---
+++ DimensionConvert/src/DimensionConverterDemo.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class DimensionConverterDemo {
+
+	public static void main(String[] args) {
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Enter a measurement in inches: ");
+		double inches = keyboard.nextDouble();
+		double feet =
+				DimensionConverter.convertInchesToFeet(inches);
+		System.out.println(inches + "inches =" + feet + "feet.");
+		System.out.print ("Enter a measurement in feet: ");
+		feet = keyboard.nextDouble();
+		inches = DimensionConverter.convertFeetToInches(feet);
+		System.out.println (feet + " feet = " + inches + " inches.");
+	}
+
+}

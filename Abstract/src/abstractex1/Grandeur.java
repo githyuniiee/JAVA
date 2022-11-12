afdd04b4592d9fe5ee3786c882740610496e4065
Bohyun Choi <109260733+githyuniiee@users.noreply.github.com>
2022-11-12 15:22:55 +0900

    Add files via upload

---
+++ Abstract/src/abstractex1/Grandeur.java
@@ -0,0 +1,27 @@
+package abstractex1;
+
+public class Grandeur extends Car {
+	@Override
+	public void start() {
+		System.out.println("Grandeur 시동을 겁니다.");
+	}
+
+	@Override
+	public void drive() {
+		System.out.println("Grandeur 달립니다.");
+		
+	}
+
+	@Override
+	public void stop() {
+		System.out.println("Grandeur 멈춥니다.");
+		
+	}
+
+	@Override
+	public void turnOff() {
+		System.out.println("Grandeur 시동을 끕니다.");
+		
+	}
+
+}

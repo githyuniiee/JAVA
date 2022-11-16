cc496ae73f27f4e1ee8169133f37d310889e26d2
Bohyun Choi <109260733+githyuniiee@users.noreply.github.com>
2022-11-16 21:21:40 +0900

    Add files via upload

---
+++ classex/PersonTest.java
@@ -0,0 +1,37 @@
+package classex;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class PersonTest {
+
+	public static void main(String[] args) {
+		
+		Class personClass = Person.class;
+		
+		Constructor[] cons = personClass.getConstructors();
+		for(Constructor c: cons) {
+			System.out.println(c);
+		}
+		
+		
+		System.out.println();
+		
+		Method[] methods = personClass.getMethods();
+		for(Method m : methods) {
+			System.out.println(m);
+		}
+		
+		
+		Field[] fields = personClass.getFields();
+		for(Field f: fields) {
+			System.out.println(f);
+			System.out.println(f.getName());
+			
+		}
+		
+
+	}
+
+}
